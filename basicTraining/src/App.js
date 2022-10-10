951a5cd5bc61742fb2c70bfa22b7cc52a762b91c
xzhang49 <xzhanguh@gmail.com>
2022-10-10 10:07:04 -0500

    8th commit

--- basicTraining/src/App.js
+++ basicTraining/src/App.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [cityName, setCityName] = useState('');
+  const [cities, setCities] = useState([]);
+
+  const cityNameHandler = (e) => setCityName(e.target.value);
+
+  const citiesHandler = () => setCities([...cities, cityName]);
+
   return (
     <div className="App">
-      <input />
+      <input onChange={cityNameHandler} value={cityName} />
       <div>
-        <button>add city</button>
+        <button onClick={citiesHandler}>add city</button>
       </div>
+      {cities.map(city => (
+        <div>{city}</div>
+      ))}
+
+      <h1>Hello StackBlitz</h1>
+      <p>Start editing to see some magic happen :)</p>
     </div>
   );
 }
