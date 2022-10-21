3e2ae848e433019db5cb79f474a981ed9a219f1b
xzhang49 <xzhanguh@gmail.com>
2022-10-20 23:45:33 -0500

    10th commit

--- form_validation/src/App.js
+++ form_validation/src/App.js
@@ -4,7 +4,51 @@ import FormInput from './components/FormInput';
 import "./app.css";
 
 function App() {
-  const [username, setUsername] = useState("");
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      label: "Username"
+    },
+    {
+      id: 2,
+      email: "email",
+      type: "text",
+      placeholder: "Email",
+      label: "Email"
+    },
+    {
+      id: 3,
+      name: "brithday",
+      type: "text",
+      placeholder: "Birthday",
+      label: "Birthday"
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      label: "Password"
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      label: "Confirm Password"
+    },
+  ]
 
   const submitHandler = (e) => {
     e.preventDefault();
