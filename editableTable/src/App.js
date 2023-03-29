3fe5a09eef9134c4dba3ccb04feda9d4fd719d18
xzhang49 <xzhanguh@gmail.com>
2023-03-29 12:38:01 -0500

    Editable Table

--- editableTable/src/App.js
+++ editableTable/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import { Table } from "./components/Table";
+import { Modal } from "./components/Modal";
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      page: "Home",
+      description: "This is the main page of the website",
+      status: "live",
+    },
+    {
+      page: "About Us",
+      description: "This page has details about the company",
+      status: "draft",
+    },
+    {
+      page: "Pricing",
+      description: "Prices for different subscriptions",
+      status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if(idx !== rowToEdit) return currRow;
+            return newRow;
+          })
+        );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+      <button onClick={() => setModalOpen(true)} className="btn">
+        Add
+      </button>
+      {modalOpen && (
+        <Modal 
+          closeModal={() => {
+            setModalOpen(false);
+            setRowToEdit(null);
+          }}
+          onSubmit={handleSubmit}
+          defaultValue={rowToEdit !== null && rows[rowToEdit]}
+        />
+      )}
     </div>
   );
 }
